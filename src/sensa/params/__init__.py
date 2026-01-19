48d56961c76b0da2d7deb2f21763ab59eb4ad42b
VG <vikas11187@gmail.com>
2026-01-18 22:57:16 -0600

    norm params

--- src/sensa/params/__init__.py
+++ src/sensa/params/__init__.py
@@ -1,9 +1,20 @@
 from sensa.params.base import BaseParams
 from sensa.params.data import DataParams
+from sensa.params.layer import ActivationParams, Norm2dParams, NormParams
 from sensa.params.loss import LossParams
 from sensa.params.model import ModelParams
 from sensa.params.optim import OptimParams
 from sensa.params.trainer import TrainerParams
 
 
-__all__ = ["BaseParams", "DataParams", "LossParams", "ModelParams", "OptimParams", "TrainerParams"]
+__all__ = [
+    "ActivationParams",
+    "BaseParams",
+    "DataParams",
+    "LossParams",
+    "ModelParams",
+    "Norm2dParams",
+    "NormParams",
+    "OptimParams",
+    "TrainerParams",
+]
--- src/sensa/params/layer.py
+++ src/sensa/params/layer.py
@@ -3,10 +3,11 @@ from collections.abc import Callable
 
 import pydantic
 import torch
+import torchvision as tv
 
 
-class ActivationConfig(pydantic.BaseModel):
-    """Activation configuration.
+class ActivationParams(pydantic.BaseModel):
+    """Activation parameters.
 
     Parameters:
         act_layer (Callable[..., torch.nn.Module], optional):
@@ -33,6 +34,84 @@ class ActivationConfig(pydantic.BaseModel):
     @pydantic.field_serializer("act_layer", when_used="json")
     def serialize_act_layer(self, value: Callable[..., torch.nn.Module]):
         """Serialize act_layer for JSON."""
+        if inspect.isclass(value) and issubclass(value, torch.nn.Module):
+            return value.__name__
+        return str(value)
+
+
+class NormParams(pydantic.BaseModel):
+    """Normalization parameters.
+
+    Parameters:
+        norm_layer (Callable[..., torch.nn.Module] | str, optional):
+            Normalization layer for the stem blocks. Defaults "layernorm".
+    """
+
+    norm_layer: Callable[..., torch.nn.Module]
+
+    @pydantic.field_validator("norm_layer", mode="before")
+    @classmethod
+    def validate_norm_layer(cls, data: Callable[..., torch.nn.Module] | str) -> Callable[..., torch.nn.Module]:
+        """Validate the normalization layer."""
+        match data:
+            case "batchnorm":
+                data = torch.nn.BatchNorm1d
+            case "dyt":
+                from sensa.layers.dyt import DyT
+
+                data = DyT
+            case "instancenorm":
+                data = torch.nn.InstanceNorm1d
+            case "layernorm":
+                data = torch.nn.LayerNorm
+            case "rmsnorm":
+                from sensa.layers.rmsnorm import RMSNorm
+
+                data = RMSNorm
+        return data
+
+    @pydantic.field_serializer("norm_layer", when_used="json")
+    def serialize_norm_layer(self, value: Callable[..., torch.nn.Module]):
+        """Serialize norm_layer for JSON."""
+        if inspect.isclass(value) and issubclass(value, torch.nn.Module):
+            return value.__name__
+        return str(value)
+
+
+class Norm2dParams(pydantic.BaseModel):
+    """2D Normalization parameters.
+
+    Parameters:
+        norm_layer (Callable[..., torch.nn.Module] | str):
+            Normalization layer for the stem blocks. Defaults "layernorm".
+    """
+
+    norm_layer: Callable[..., torch.nn.Module]
+
+    @pydantic.field_validator("norm_layer", mode="before")
+    @classmethod
+    def validate_norm_layer(cls, data: Callable[..., torch.nn.Module] | str) -> Callable[..., torch.nn.Module]:
+        """Validate the normalization layer."""
+        match data:
+            case "batchnorm":
+                data = torch.nn.BatchNorm2d
+            case "dyt":
+                from sensa.layers.dyt import DyT2D
+
+                data = DyT2D
+            case "instancenorm":
+                data = torch.nn.InstanceNorm2d
+            case "layernorm":
+                data = tv.models.convnext.LayerNorm2d
+            case "rmsnorm":
+                from sensa.layers.rmsnorm import RMSNorm2D
+
+                data = RMSNorm2D
+        return data
+
+    @pydantic.field_serializer("norm_layer", when_used="json")
+    def serialize_norm_layer(self, value: Callable[..., torch.nn.Module]):
+        """Serialize norm_layer for JSON."""
         if inspect.isclass(value) and issubclass(value, torch.nn.Module):
             return value.__name__
         return str(value)
